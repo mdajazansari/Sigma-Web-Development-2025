bef274536db6007234c0e6fe4af955cf78d032f0
mdajazansari <mdajazansari2912@gmail.com>
2025-11-05 23:18:58 +0530

    Var and rules for choosing variable name

---
+++ Lecture 55/script.js
@@ -0,0 +1,10 @@
+console.log("This is lecture 55");
+
+var a = 5;
+var b = 6;
+var c = "Ajaz";
+
+console.log(a+b);
+console.log(a+b+8);
+console.log(c);
+console.log(typeof(a), typeof(b), typeof(c));
