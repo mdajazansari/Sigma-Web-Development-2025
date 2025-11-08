3e35c48661ab1683b814819e36191be8f61d789c
mdajazansari <mdajazansari2912@gmail.com>
2025-11-08 10:41:11 +0530

    Lecture 55

--- Lecture 55/script.js
+++ Lecture 55/script.js
@@ -1,10 +1,39 @@
-console.log("This is lecture 55");
+// // console.log("This is lecture 55");
 
-var a = 5;
-var b = 6;
-var c = "Ajaz";
+// // var a = 5;
+// // var b = 6;
+// // var c = "Ajaz";
 
-console.log(a+b);
-console.log(a+b+8);
-console.log(c);
-console.log(typeof(a), typeof(b), typeof(c));
+// // console.log(a+b);
+// // console.log(a+b+8);
+// // console.log(c);
+// // console.log(typeof(a), typeof(b), typeof(c));
+
+
+// // const d = 5;
+// // d = d+1;
+
+
+
+// var a = 10;
+
+// {
+//     let a = 20;
+//     console.log(a);
+// }
+
+// console.log(a)
+
+
+let a = "Ajaz";
+let b = 2;
+let c = 3.4;
+const d = true;
+let e = undefined;
+let f = null;
+
+console.log (a, b, c, d, e, f);
+console.log (typeof(a), typeof(b), typeof(c), typeof(d), typeof(e), typeof(f));
+
+
+ 
