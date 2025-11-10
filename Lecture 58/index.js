1dfb7a999d31560dd6aba8ead8216aa52a4c06f3
mdajazansari <mdajazansari2912@gmail.com>
2025-11-10 09:55:26 +0530

    L 58 Functions

---
+++ Lecture 58/index.js
@@ -0,0 +1,84 @@
+/*Simple Function
+
+function nice (name) {
+    console.log("Hey " + name + " you are nice");
+    console.log("Hey " + name + " you car is wondreful");
+    console.log("Hey " + name + " your tshirt is great");
+    console.log("Hey " + name + " you are smart");
+   
+}
+
+nice ("Ajaz")
+
+*/
+
+
+/* Sum Function
+
+function sum (a,b){
+    console.log(a + b);
+}
+
+sum (3,5)
+
+*/
+
+/* Return function
+
+function sum (a,b){
+    return a+b;
+}
+
+result1 = sum(2,8);
+result2 = sum(12,18);
+
+console.log("Sum of numbers are", result1, result2);
+
+*/
+
+/* Real life example
+
+function makePizza(){
+    let pizza = "Cheese pizza";
+    return pizza;
+}
+
+let myOrder = makePizza();
+console.log("Order Received:", myOrder);
+
+*/
+
+
+
+/*
+🧠 Question:
+
+Ek function likho multiply(a, b) jo do numbers ko multiply kare
+aur result return kare (print nahi).
+
+Phir us function se 3 results nikalo:
+
+2 * 3
+
+5 * 4
+
+10 * 2
+
+Aur sab results ko ek hi line me print karo.
+
+📝 Expected Output:
+Multiplications are 6 20 20?
+
+*/
+
+function multiply (a, b){
+    let result = a * b;
+    return result;
+}
+
+let result1 = multiply(2, 3);
+let result2 = multiply(5, 4);
+let result3 = multiply(10, 2);
+
+console.log("Multiplications are", result1, result2, result3);
+
